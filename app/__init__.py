1e504da7a9bcb3d621aaae94f3102cf0a1d373f5
Pedro Sousa <ppls2106@gmail.com>
2020-04-12 05:00:14 -0300

    add cliente_model

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,23 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config.from_object('config')
+from config import Config
 
-db = SQLAlchemy(app=app)
+db = SQLAlchemy()
+migrate = Migrate()
 
-from app.views import view_bp
 
-app.register_blueprint(view_bp)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config())
+
+    db.init_app(app=app)
+    migrate.init_app(app=app, db=db)
+
+    from app.views import view_bp
+
+    app.register_blueprint(view_bp)
+
+    from app.models import cliente_model
+    return app
--- config.py
+++ config.py
@@ -1,5 +1,8 @@
-USERNAME = 'iec_desenv'
-PASSWORD = 'iec_desenv'
-SERVER = 'localhost'
-DB = 'flask_fundamentos'
-SQLALCHEMY_DATABASE_URI = f'mysql://{USERNAME}:{PASSWORD}@{SERVER}/{DB}'
+class Config:
+    SECRET_KEY = 'Miguel@12345'
+    USERNAME = 'iec_desenv'
+    PASSWORD = 'iec_desenv'
+    SERVER = 'localhost'
+    DB = 'flask_fundamentos'
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{USERNAME}:{PASSWORD}@{SERVER}/{DB}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
